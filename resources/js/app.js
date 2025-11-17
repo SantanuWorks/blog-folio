d1f237f9d247b8b7cbedc4420fb76d1ebd4432b3
Santanu Pradhan <santanuupradhaan@gmail.com>
2025-11-17 23:50:36 +0530

    added initial project setup

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import '../css/app.css';
+import './bootstrap';
+import { createApp } from 'vue';
+import ExampleComponent from './Components/ExampleComponent.vue';
+
+const app = createApp({});
+app.component('example-component', ExampleComponent);
+app.mount('#app');
+
