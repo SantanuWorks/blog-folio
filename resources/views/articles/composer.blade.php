ad688aa849984a23f4c579cf897178153e1cf769
Santanu Pradhan <santanuupradhaan@gmail.com>
2025-12-10 00:20:03 +0530

    Added Blog Compose Blade Page and Vue Component

---
+++ resources/views/articles/composer.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Composer — Write your blog')
+
+@section('content')
+    <div id="article-composer">
+        <article-composer />
+    </div>
+@endsection
