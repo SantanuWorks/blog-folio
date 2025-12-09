dfd730c2a2852cd81f63af1843c3d07da85f687f
Santanu Pradhan <santanuupradhaan@gmail.com>
2025-12-09 22:17:57 +0530

    added routes setup for UI pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Articles routes
+Route::get('/articles', function () { return view('articles.home'); })->name('articles.home');
+Route::get('/articles/all', function () { return view('articles.index'); })->name('articles.index');
+Route::get('/articles/composer', function () { return view('articles.composer'); })->name('articles.composer');
+Route::get('/articles/{slug}', function () { return view('articles.view'); })->name('articles.view');
+
+// Portfolio routes
+Route::get('/', function () { return view('portfolio.me'); })->name('portfolio.me');
+
+// Fallback route 
+Route::fallback(function () { return response()->view('errors.404', [], 404); });
