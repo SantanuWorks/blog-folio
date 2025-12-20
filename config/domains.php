75eedf5439ab0d65b8387ae822910b0b1e9fb06c
Santanu Pradhan <santanuupradhaan@gmail.com>
2025-12-20 18:28:18 +0530

    Added env-based domains, redesigned with domain based routing, updated vite config for local-dev, updated gitignore

---
+++ config/domains.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'portfolio' => env('PORTFOLIO_DOMAIN'),
+    'articles' => env('ARTICLES_DOMAIN'),
+    'admin' => env('ADMIN_DOMAIN'),
+];
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Articles routes
-Route::get('/articles', function () { return view('articles.home'); })->name('articles.home');
-Route::get('/articles/all', function () { return view('articles.index'); })->name('articles.index');
-Route::get('/articles/composer', function () { return view('articles.composer'); })->name('articles.composer');
-Route::get('/articles/{slug}', function () { return view('articles.view'); })->name('articles.view');
+// Portfolio Domain
+Route::domain(config('domains.portfolio'))->group(function () {
+    Route::get('/', function () {
+        return view('portfolio.me');
+    })->name('portfolio.me');
+});
 
-// Portfolio routes
-Route::get('/', function () { return view('portfolio.me'); })->name('portfolio.me');
+// Articles Domain
+Route::domain(config('domains.articles'))->group(function () {
+    Route::get('/', function () {
+        return view('articles.home');
+    })->name('articles.home');
+    Route::get('/{slug}', function () {
+        return view('articles.view');
+    })->name('articles.view');
+});
+
+// Admin Domain
+Route::domain(config('domains.admin'))->group(function () {
+    Route::get('/articles/all', function () {
+        return view('articles.index');
+    })->name('articles.index');
+    Route::get('/articles/composer', function () {
+        return view('articles.composer');
+    })->name('articles.composer');
+});
 
 // Fallback route 
-Route::fallback(function () { return response()->view('errors.404', [], 404); });
+Route::fallback(function () {
+    return response()->view('errors.404', [], 404);
+});
